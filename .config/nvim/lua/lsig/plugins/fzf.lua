3207b7269561dfc8509ec44ff3e5296246490c10
Lsig <logis21@ru.is>
2024-06-09 23:44:14 +0000

    feat: Add new keymaps for fzf

--- .config/nvim/lua/lsig/plugins/fzf.lua
+++ .config/nvim/lua/lsig/plugins/fzf.lua
@@ -18,6 +18,13 @@ return {
       desc = "Search command history",
     },
     {
+      "<leader>/",
+      function()
+        require("fzf-lua").lgrep_curbuf()
+      end,
+      desc = "Search command history",
+    },
+    {
       "<leader>sf",
       function()
         require("fzf-lua").files()
@@ -59,20 +66,40 @@ return {
       end,
       desc = "Resume FZF",
     },
+    {
+      "<leader>gs",
+      function()
+        require("fzf-lua").git_status()
+      end,
+      desc = "Show git status",
+    },
   },
   config = function()
     -- calling `setup` is optional for customization
     local fzf = require("fzf-lua")
+    local actions = require("fzf-lua.actions")
     fzf.setup({
       keymap = {
         fzf = {
           ["CTRL-Q"] = "select-all+accept",
         },
       },
+      git = {
+        status = {
+          actions = {
+            -- actions inherit from 'actions.files' and merge
+            ["right"] = false,
+            ["left"] = false,
+            ["ctrl-l"] = { fn = actions.git_unstage, reload = true },
+            ["ctrl-h"] = { fn = actions.git_stage, reload = true },
+            ["ctrl-x"] = { fn = actions.git_reset, reload = true },
+          },
+        },
+      },
     })
     fzf.register_ui_select()
 
-    local ts_group = vim.api.nvim_create_augroup("TelescopeOnEnter", { clear = true })
+    local ts_group = vim.api.nvim_create_augroup("FzfLuanOnEnter", { clear = true })
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
       callback = function()
         local first_arg = vim.v.argv[3]
@@ -86,143 +113,3 @@ return {
     })
   end,
 }
--- return {
---   {
---     "nvim-telescope/telescope.nvim",
---     version = "*",
---     dependencies = { { "nvim-lua/plenary.nvim" } },
---     config = function()
---       local actions = require("telescope.actions")
---       require("telescope").setup({
---         defaults = {
---           file_ignore_patterns = {
---             "node_modules",
---             ".git",
---             "dist",
---             "yarn.lock",
---             "profile_list_images",
---             "target",
---           },
---           sorting_strategy = "ascending",
---           multi_icon = "",
---           layout_strategy = "flex",
---           winblend = 0,
---           layout_config = {
---             horizontal = {
---               prompt_position = "top",
---               preview_width = 0.5,
---             },
---             vertical = {
---               mirror = true,
---             },
---             center = {
---               mirror = true,
---             },
---           },
---           hl_result_eol = false,
---           preview = {
---             msg_bg_fillchar = " ",
---           },
---           history = {
---             cycle_wrap = true,
---           },
---           cache = false,
---           dynamic_preview_title = true,
---           mappings = {
---             i = {
---               ["<C-w>"] = "which_key",
---             },
---           },
---         },
---         pickers = {
---           find_files = {
---             previewer = true,
---             mappings = {
---               i = {
---                 ["<esc>"] = actions.close,
---               },
---             },
---           },
---           buffers = {
---             theme = "dropdown",
---             previewer = false,
---             mappings = {
---               i = {
---                 ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
---                 ["<esc>"] = actions.close,
---               },
---             },
---             sort_mru = true,
---           },
---           resume = {
---             theme = "dropdown",
---             previewer = false,
---           },
---           current_buffer_fuzzy_find = {
---             theme = "dropdown",
---             previewer = false,
---             mappings = {
---               i = {
---                 ["<esc>"] = actions.close,
---                 ["<CR>"] = function(prompt_bufnr)
---                   actions.select_default(prompt_bufnr)
---                   vim.cmd("normal! zz")
---                 end,
---               },
---             },
---           },
---           git_files = {
---             theme = "dropdown",
---             previewer = false,
---             show_untracked = true,
---             mappings = {
---               i = {
---                 ["<esc>"] = actions.close,
---               },
---             },
---           },
---         },
---         extensions = {
---           fzf = {
---             fuzzy = true, -- false will only do exact matching
---             override_generic_sorter = true, -- override the generic sorter
---             override_file_sorter = true, -- override the file sorter
---             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
---             -- the default case_mode is "smart_case"
---           },
---           ["ui-select"] = {
---             require("telescope.themes").get_cursor({}),
---           },
---         },
---       })
---
---       -- Open Telescope on startup if the first argument is a directory
---       local ts_group = vim.api.nvim_create_augroup("TelescopeOnEnter", { clear = true })
---       vim.api.nvim_create_autocmd({ "VimEnter" }, {
---         callback = function()
---           local first_arg = vim.v.argv[3]
---           if first_arg and vim.fn.isdirectory(first_arg) == 1 then
---             -- Vim creates a buffer for folder. Close it.
---             vim.cmd(":bd 1")
---             require("telescope.builtin").find_files({ search_dirs = { first_arg } })
---           end
---         end,
---         group = ts_group,
---       })
---
---       require("telescope").load_extension("fzf")
---       require("telescope").load_extension("ui-select")
---     end,
---   },
---   {
---     "nvim-telescope/telescope-ui-select.nvim",
---     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
---   },
---   {
---     "nvim-telescope/telescope-fzf-native.nvim",
---     build = "make",
---     cond = function()
---       return vim.fn.executable("make") == 1
---     end,
---   },
--- }
