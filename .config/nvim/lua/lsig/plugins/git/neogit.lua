ea5519bc069df2aab73f4032f9cc5106c6d7b55b
Lsig <logis21@ru.is>
2024-06-09 23:56:08 +0000

    feat: Update and switch git client

--- .config/nvim/lua/lsig/plugins/git/neogit.lua
+++ .config/nvim/lua/lsig/plugins/git/neogit.lua
@@ -1,15 +1,3 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- Diff integration
-    "ibhagwan/fzf-lua",
-    -- "nvim-telescope/telescope.nvim",
-  },
-  cmd = "Neogit",
-  keys = {
-    { "<Leader>gs", "<cmd>Neogit kind=vsplit<CR>", desc = "Neogit" },
-  },
-  config = true,
-  event = "BufEnter",
+  { "echasnovski/mini-git", version = false, main = "mini.git", opts = {} },
 }
