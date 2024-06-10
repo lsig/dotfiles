659b948ebb2ebfc4ef3217e845aa1f114cd6af8e
lsig-ks <logi@keystrike.com>
2024-06-10 14:01:31 +0000

    feat: Setup alacritty

--- .config/nvim/lua/lsig/plugins/fzf.lua
+++ .config/nvim/lua/lsig/plugins/fzf.lua
@@ -99,7 +99,7 @@ return {
     })
     fzf.register_ui_select()
 
-    local ts_group = vim.api.nvim_create_augroup("FzfLuanOnEnter", { clear = true })
+    local ts_group = vim.api.nvim_create_augroup("FzfLuaOnEnter", { clear = true })
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
       callback = function()
         local first_arg = vim.v.argv[3]
