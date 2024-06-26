acbf4fc525c84c373dd91fa48c632e8ba60991cc
Lsig <logis21@ru.is>
2024-06-26 22:10:22 +0000

    feat: Change ui select

--- .config/nvim/lua/lsig/plugins/fzf.lua
+++ .config/nvim/lua/lsig/plugins/fzf.lua
@@ -116,16 +116,7 @@ return {
         },
       },
     })
-    fzf.register_ui_select(function(_, items)
-      local min_h, max_h = 0.15, 0.70
-      local h = (#items + 4) / vim.o.lines
-      if h < min_h then
-        h = min_h
-      elseif h > max_h then
-        h = max_h
-      end
-      return { winopts = { height = h, width = 0.60, row = 0.40 } }
-    end)
+    fzf.register_ui_select()
 
     local ts_group = vim.api.nvim_create_augroup("FzfLuaOnEnter", { clear = true })
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
