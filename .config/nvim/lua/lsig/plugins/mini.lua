472ff81d241343025e907eda2ef64deea3f6806c
lsig-ks <logi@keystrike.com>
2024-06-26 09:14:30 +0000

    feat: Remove noice use mini instead

--- .config/nvim/lua/lsig/plugins/mini.lua
+++ .config/nvim/lua/lsig/plugins/mini.lua
@@ -12,6 +12,13 @@ return {
       require("mini.surround").setup()
       require("mini.git").setup()
       require("mini.ai").setup()
+      local notify = require("mini.notify")
+      notify.setup()
+      vim.notify = notify.make_notify({
+        ERROR = { duration = 5000 },
+        WARN = { duration = 4000 },
+        INFO = { duration = 3000 },
+      })
     end,
   },
 }
