7b485fbab0dd1905563656a9ce6fbc67993231ba
Lsig <logis21@ru.is>
2024-07-04 20:19:52 +0000

    feat: Remove nvim-devicons for mini.icons

--- .config/nvim/lua/lsig/plugins/misc.lua
+++ .config/nvim/lua/lsig/plugins/misc.lua
@@ -10,6 +10,16 @@ return {
       skip_confirm_for_simple_edits = true,
       keymaps = { ["<C-t>"] = false },
     },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {},
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
   },
 }
