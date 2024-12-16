ab0382e3bffdafbc87142257bfef1ea6c2c0ab53
Lsig <logis21@ru.is>
2024-12-15 22:14:52 -0500

    feat: Add term command

--- .config/nvim/lua/lsig/plugins/misc.lua
+++ .config/nvim/lua/lsig/plugins/misc.lua
@@ -1,3 +1,12 @@
+-- Disable line numbers inside terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
 return {
   { "tpope/vim-sleuth", event = "VeryLazy" },
   { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
